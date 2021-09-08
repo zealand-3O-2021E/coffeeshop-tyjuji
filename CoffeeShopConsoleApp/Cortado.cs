17fc21d987cb35392298a3e4ba8a1fa92724d891
Kristian Ib Thiesson <kris89r8@edu.easj.dk>
2021-09-08 21:11:17 +0200

    Implemented coffees and changed the base class to abstract

---
+++ CoffeeShopConsoleApp/Cortado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleApp
+{
+    class Cortado : Coffee
+    {
+        public override int price()
+        {
+            return 25;
+        }
+
+        public override string Strength()
+        {
+            return "Medium";
+        }
+    }
+}
---
+++ CoffeeShopConsoleApp/Latte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleApp
+{
+    class Latte : Coffee
+    {
+        public override int price()
+        {
+            return 40;
+        }
+
+        public override string Strength()
+        {
+            return "Weak";
+        }
+    }
+}
